43030f7cfebad56ec421db61c7c11f064ba34405
Sandul Renuja <sandulrenuja@gmail.com>
2022-08-01 23:05:19 +0530

    Add answer for Tutorial 4 - Q2

---
+++ tutorial-4/Q2.scala
@@ -0,0 +1,12 @@
+object Q2 {
+  def main(args: Array[String]): Unit = {
+    print("Enter a number: ");
+    println(myFunc(scala.io.StdIn.readInt()));
+  }
+
+  def myFunc(input: Int): String = input match {
+    case x if x <= 0     => "Negative/Zero is input"
+    case x if x % 2 == 0 => "Even number is given"
+    case _               => "Odd number is given"
+  }
+}
