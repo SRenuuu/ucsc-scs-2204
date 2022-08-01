7f422ef17a5d684dec40584ca523e215561d3c17
Sandul Renuja <sandulrenuja@gmail.com>
2022-08-01 23:32:37 +0530

    Add answer for Tutorial 4 - Q3

---
+++ tutorial-4/Q3.scala
@@ -0,0 +1,24 @@
+object Q3 {
+  def main(args: Array[String]): Unit = {
+    println(formatNames("Benny")(toUpper))
+    println(
+      formatNames("Niroshan")((name: String) =>
+        name.substring(0, 1) + toUpper(name.substring(1, 2)) + name.substring(2)
+      )
+    )
+    println(formatNames("Saman")(toLower))
+    println(
+      formatNames("Kumara")((name: String) =>
+        name.substring(0, name.length() - 1) + toUpper(
+          name.substring(name.length() - 1, name.length())
+        )
+      )
+    )
+  }
+
+  def toUpper(input: String): String = input.toUpperCase()
+  def toLower(input: String): String = input.toLowerCase()
+  def formatNames(name: String)(fn: String => String): String = fn(
+    name
+  )
+}
