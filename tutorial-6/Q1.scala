f6ac5993d89b94fe4a01a5ff7d7d1dd7771245ac
Sandul Renuja <sandulrenuja@gmail.com>
2022-09-06 08:21:17 +0530

    Update answer for Tutorial 6 - Q1

--- tutorial-6/Q1.scala
+++ tutorial-6/Q1.scala
@@ -1,26 +1,31 @@
 // Tutorial 6 - Question 1
 
 object Q1 {
-  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
   val key = 5
 
   def main(args: Array[String]): Unit = {
-    val cipherText = cipher(encrypt, "ABC", key, alphabet)
+    val cipherText = cipher(encrypt, "HelloWorld", key, alphabet)
     val plainText = cipher(decrypt, cipherText, key, alphabet)
 
     printf("Cipher Text : %s\n", cipherText)
     printf("Plain Text : %s\n", plainText)
   }
 
-  // Function that encrypts data using Caesar Cipher
-  val encrypt = (c: Char, key: Int, str: String) =>
-    str((str.indexOf(c.toUpper) + key) % str.size)
+  // Function that encrypts a character using Caesar Cipher
+  val encrypt = (c: Char, key: Int, alphabet: String) =>
+    alphabet((alphabet.indexOf(c.toUpper) + key) % alphabet.size)
 
-  // Function that decrypts data using Caesar Cipher
+  // Function that decrypts a character using Caesar Cipher
   val decrypt = (c: Char, key: Int, str: String) => encrypt(c, -key, str)
 
   // HOF that takes encrypt/decrypt function and process the data
+  // It applies the algo to each character of the string
   val cipher =
-    (algo: (Char, Int, String) => Char, s: String, key: Int, a: String) =>
-      s.map(algo(_, key, a))
+    (
+        algo: (Char, Int, String) => Char,
+        text: String,
+        key: Int,
+        alphabet: String
+    ) => text.map(algo(_, key, alphabet))
 }
